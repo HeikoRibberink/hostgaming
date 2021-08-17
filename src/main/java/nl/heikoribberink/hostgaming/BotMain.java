47ccf4692030b4f74eccdd4da01d6998a7ae80a1
BlijdeAankomst1896 <heiko.ribberink@deblijdeaankomst.nl>
2021-08-17 14:24:33 +0200

    Created BotMain

--- src/main/java/nl/heikoribberink/hostgaming/BotMain.java
+++ src/main/java/nl/heikoribberink/hostgaming/BotMain.java
@@ -1,7 +1,13 @@
 package nl.heikoribberink.hostgaming;
 
+import discord4j.core.DiscordClient;
+import discord4j.core.GatewayDiscordClient;
+
 public class BotMain {
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		final String token = args[0];
+		DiscordClient client = DiscordClient.create(token);
+		GatewayDiscordClient gateway = client.login().block();
+		
 	}
 }
