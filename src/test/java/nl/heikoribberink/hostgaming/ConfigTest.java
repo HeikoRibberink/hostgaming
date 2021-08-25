20847b9ed357ea56b4033d1ec3615bf5c1da4733
MichaPehlivan <michapehlivan2004@gmail.com>
2021-08-25 16:39:44 +0200

    updated ConfigTest.java with exit key value

--- src/test/java/nl/heikoribberink/hostgaming/ConfigTest.java
+++ src/test/java/nl/heikoribberink/hostgaming/ConfigTest.java
@@ -18,6 +18,7 @@ public class ConfigTest {
         System.out.println("host id: " + config.getHost());
         System.out.println("event title:" + config.getEventTitle());
         System.out.println("mode: " + config.getMode());
+        System.out.println("exit key: " + config.getExitKey());
         System.out.println("whitelisted users: " + config.getWhitelistedUsers());
         System.out.println("key mappings: " + config.getKeyMappings());
     }
